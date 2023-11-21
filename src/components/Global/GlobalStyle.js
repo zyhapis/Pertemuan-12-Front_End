abbb1c6f81f2ca18055d079eb3e73089b1b4f675
Hafidz Fatah K.J <hellowould991@gmail.com>
2023-11-21 21:02:37 +0700

    tugas-pertemuan-12

---
+++ src/components/Global/GlobalStyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+* {
+    margin: 0;
+    padding: 0;
+    font-family: 'Lexend', sans-serif;
+}
+
+body {
+    background-color: #221f1f;
+    margin: 0;
+}
+`;
+
+export default GlobalStyle;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,91 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Navbar = () => {
+  return (
+    <NavbarContainer>
+      <NavbarStyle>
+        <div>
+          <h1 className="navbar__brand">My Movie</h1>
+        </div>
+        <div>
+          <ul className="navbar__list">
+            <li className="navbar__item">
+              <Link className="nav__title" to={"/"}>
+                Home
+              </Link>
+            </li>
+            <li className="navbar__item">
+              <Link className="nav__title" to={"/add"}>
+                Add Movie
+              </Link>
+            </li>
+            <li className="navbar__item">
+              <Link className="nav__title" to={"/popular"}>
+                Popular
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </NavbarStyle>
+    </NavbarContainer>
+  );
+};
+
+const NavbarStyle = styled.nav`
+  display: flex;
+  flex-direction: column;
+
+  .navbar__brand {
+    font-size: 2.4rem;
+    margin-bottom: 1rem;
+    margin: 0 auto;
+    color: #e50914;
+  }
+
+  .navbar__list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    text-align: center;
+  }
+
+  .navbar__item {
+    margin-bottom: 1rem;
+  }
+
+  .nav__title {
+    color: #f5f5f1;
+    text-decoration: none;
+  }
+
+  /* Medium Screen */
+  @media (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    .navbar__brand {
+      margin-bottom: 0;
+    }
+
+    .navbar__list {
+      flex-direction: row;
+    }
+
+    .navbar__item {
+      margin: 0 1rem;
+    }
+  }
+
+  /* Large Screen */
+  @media (min-width: 992px) {
+  }
+`;
+
+const NavbarContainer = styled.div`
+  background-color: #221f1f;
+  padding: 1rem;
+`;
+
+export default Navbar;
